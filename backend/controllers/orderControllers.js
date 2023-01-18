05ce22d060e3897af07e63dbb50d6e75cafea5b9
ismailcet <ismailcet3@gmail.com>
2023-01-18 23:01:10 +0300

    Backend Side is over.Just Payment functions are not finished.

--- backend/controllers/orderControllers.js
+++ backend/controllers/orderControllers.js
@@ -30,4 +30,58 @@ const createOrder = async (request, response) => {
 };
 
 //GetAll order will added.
-module.exports = { createOrder };
+const getAllOrder = (request, response) => {
+  try {
+    pool.query(
+      'SELECT ord.id,ord.quantity,ord.size,pro.image,pro.name,pro.description,pro.price,usr.firstname,usr.surname,usr.email,usr.city,usr.country,usr.address1,usr.phonenumber,orders.payment_info,orders.ordered_date FROM public."orderDetails" ord INNER JOIN products pro ON pro.id=ord.product_id INNER JOIN users usr ON usr.id=ord.user_id INNER JOIN orders ON orders.id=ord.order_id',
+      (error, results) => {
+        if (error) {
+          response.status(400).json(error);
+        }
+
+        response.status(200).json(results.rows);
+      }
+    );
+  } catch (error) {
+    response.status(400).json(error);
+  }
+};
+
+//Get Order by user
+const getOrderByUserId = (request, response) => {
+  const id = request.params.id;
+  try {
+    pool.query(
+      'SELECT ord.id,ord.quantity,ord.size,ord.shipping,pro.image,pro.name,pro.description,pro.price,usr.firstname,usr.surname,usr.email,usr.city,usr.country,usr.address1,usr.phonenumber,orders.payment_info,orders.ordered_date FROM public."orderDetails" ord INNER JOIN products pro ON pro.id=ord.product_id INNER JOIN users usr ON usr.id=ord.user_id INNER JOIN orders ON orders.id=ord.order_id WHERE ord.user_id=$1',
+      [id],
+      (error, results) => {
+        if (error) {
+          response.status(400).json(error);
+        }
+        response.status(200).json(results.rows);
+      }
+    );
+  } catch (error) {
+    response.status(400).json(error);
+  }
+};
+//Get Order by OrderId
+const getOrderById = (request, response) => {
+  const id = request.params.id;
+  try {
+    pool.query(
+      'SELECT ord.id,ord.quantity,ord.size,ord.shipping,pro.image,pro.name,pro.description,pro.price,usr.firstname,usr.surname,usr.email,usr.city,usr.country,usr.address1,usr.phonenumber,orders.payment_info,orders.ordered_date FROM public."orderDetails" ord INNER JOIN products pro ON pro.id=ord.product_id INNER JOIN users usr ON usr.id=ord.user_id INNER JOIN orders ON orders.id=ord.order_id WHERE ord.id=$1',
+      [id],
+      (error, results) => {
+        if (error) {
+          response.status(400).json(error);
+        }
+        response.status(200).json(results.rows);
+      }
+    );
+  } catch (error) {
+    response.status(400).json(error);
+  }
+};
+
+module.exports = { createOrder, getAllOrder, getOrderByUserId, getOrderById };
--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -3,5 +3,8 @@ const router = express.Router();
 const orderControllers = require("../controllers/orderControllers");
 
 router.route("/").post(orderControllers.createOrder);
+router.route("/").get(orderControllers.getAllOrder);
+router.route("/user/:id").get(orderControllers.getOrderByUserId);
+router.route("/find/:id").get(orderControllers.getOrderById);
 
 module.exports = router;
